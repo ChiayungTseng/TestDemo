1688a3f68ee3afd823693c390c40280612c2073d
zhengjiarong <zhengjiarong@iiasaas.com>
2019-12-20 17:17:44 +0800

    avro、plantUml

---
+++ src/zookeeper/ZK.java
@@ -0,0 +1,27 @@
+package zookeeper;
+
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ZK {
+    public static String connect = "127.0.0.1:2181";
+
+    public static void main(String[] args) throws Exception {
+        ZooKeeper zkc = new ZooKeeper(connect, 50000, new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                // 收到事件通知后的回调函数（应该是我们自己的事件处理逻辑）
+                System.out.println(event.getType() + "---" + event.getPath());
+
+            }
+        });
+//        zkc.create("/test","dataSet".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        List<String> rootList =  zkc.getChildren("/",false);
+        for(String root:rootList){
+            System.out.println(root);
+        }
+    }
+
+}
